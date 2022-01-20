3b6c8baf59dcd1eb457c15a9c8116750092e4601
user <yusif.haciyev1999@gmail.com>
2022-01-21 01:24:21 +0400

    commit

---
+++ ms-api-gateway/src/main/java/com/guavapay/apigateway/MsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.guavapay.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsApiGatewayApplication.class, args);
+    }
+
+}
---
+++ ms-api-gateway/src/main/java/com/guavapay/apigateway/config/SpringCloudConfig.java
@@ -0,0 +1,28 @@
+package com.guavapay.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/ms-auth/**")
+                        .uri("http://authApp:8081/")
+                        .id("authModule"))
+
+                .route(r -> r.path("/ms-user/**")
+                        .uri("http://userApp:8082/")
+                        .id("userModule"))
+
+                .route(r -> r.path("/ms-parcel/**")
+                        .uri("http://parcelApp:8083/")
+                        .id("parcelModule"))
+                .build();
+    }
+
+}
